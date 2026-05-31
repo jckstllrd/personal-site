b9cb440ccad5d773ca6368e97d494c6238879bdb
jckstllrd <jackstallard17@gmail.com>
2026-05-31 10:01:21 +0100

    feat: add basic social links to the footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,20 @@
 import styles from "./Footer.module.css";
 
 export default function Footer() {
-  return <footer></footer>;
+  return (
+    <footer>
+      <ul className={styles.socialsList}>
+        <li>
+          <a href="https://github.com/jckstllrd">GitHub</a>
+        </li>
+        <li>
+          <a href="https://dev.to/jckstllrd">Dev.To</a>
+        </li>
+
+        <li>
+          <a href="https://x.com/jackstllrd">X</a>
+        </li>
+      </ul>
+    </footer>
+  );
 }
