34fc51757097778bc04567bc8baa498e38180c1d
jckstllrd <jackstallard17@gmail.com>
2026-05-31 09:49:14 +0100

    feat: main page layout with nav, footer and main content

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
 import { Outlet } from "react-router";
 import Nav from "@/components/Nav";
+import Footer from "./components/Footer";
+import styles from "./App.module.css";
 
 function App() {
   return (
-    <>
+    <div className={styles.layoutWrapper}>
       <Nav />
       <main>
         <Outlet />
       </main>
-    </>
+      <Footer />
+    </div>
   );
 }
 
