4552c1486be64b034e647b1924a18b510868e8ef
jckstllrd <jackstallard17@gmail.com>
2026-06-01 20:39:09 +0100

    fix: remove socials names, replace with logos

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,18 +1,14 @@
 import styles from "./Footer.module.css";
+import ghlogo from "../content/GitHub_Invertocat_White_Clearspace.png";
 
 export default function Footer() {
   return (
     <footer>
       <ul className={styles.socialsList}>
         <li>
-          <a href="https://github.com/jckstllrd">GitHub</a>
-        </li>
-        <li>
-          <a href="https://dev.to/jckstllrd">Dev.To</a>
-        </li>
-
-        <li>
-          <a href="https://x.com/jackstllrd">X</a>
+          <a href="https://github.com/jckstllrd">
+            <img src={ghlogo} alt="GitHub Logo" />
+          </a>
         </li>
       </ul>
     </footer>
