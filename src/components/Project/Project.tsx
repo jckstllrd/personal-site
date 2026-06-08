e8b2c786e37ea55b2a9c4a4abed017a28fcf72b8
jckstllrd <jackstallard17@gmail.com>
2026-06-08 20:00:20 +0100

    feat: add project card component to projects grid

---
+++ src/components/Project/Project.tsx
@@ -0,0 +1,23 @@
+import styles from "./Project.module.css";
+
+interface Props {
+  name: string;
+  desc: string;
+  liveLink: string;
+  repoLink: string;
+}
+
+function Project({ name, desc, liveLink, repoLink }: Props) {
+  return (
+    <div className={styles.projectContainer}>
+      <h2>{name}</h2>
+      <p>{desc}</p>
+      <div className={styles.projectButtons}>
+        <a href={liveLink}>live</a>
+        <a href={repoLink}>repo</a>
+      </div>
+    </div>
+  );
+}
+
+export default Project;
--- src/pages/Projects/Projects.tsx
+++ src/pages/Projects/Projects.tsx
@@ -1,9 +1,23 @@
 import Container from "@/components/Container/Container";
+import styles from "./Projects.module.css";
+import Project from "@/components/Project/Project";
 
 function Projects() {
   return (
     <Container>
-      <h1>projects</h1>
+      <div className={styles.projectPage}>
+        <h1>projects</h1>
+        <div className={styles.projects}>
+          <Project
+            name={"Habit Tracker"}
+            desc={
+              "A full stack habit tracker app build with aut, api endpoints, controlled routes and a postgresql database"
+            }
+            liveLink={"https://habit-tracker-sepia-beta.vercel.app/"}
+            repoLink={"https://github.com/jckstllrd/habit-tracker"}
+          />
+        </div>
+      </div>
     </Container>
   );
 }
