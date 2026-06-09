05748048df9c74a5638844495b15717de81f50a5
jckstllrd <jackstallard17@gmail.com>
2026-06-09 07:16:56 +0100

    feat: add a brief note in the about section

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -4,6 +4,14 @@ function About() {
   return (
     <Container>
       <h1>About</h1>
+      <p>
+        I am a CS Grad that is currently rediscovering a passion for
+        programming. I work as an AI Strategy Consultant within the AI & Data
+        practice at Accenture for Capital Market clients, working across the
+        SDLC to improve and identify the best use cases for bringing in or
+        creating AI tooling. I hope to help developers know when and how to use
+        the various different embodiments of AI in their work.
+      </p>
     </Container>
   );
 }
