4edaeee5bf0d7f3b5756235775919c601712bf4e
jckstllrd <jackstallard17@gmail.com>
2026-06-10 19:37:04 +0100

    feat: implement burger menu bar for mobile navigation

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,10 +1,16 @@
 import { Link } from "react-router";
 import styles from "./Nav.module.css";
+import { useState } from "react";
 
 function Nav() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    console.log("clicked");
+    setMenuOpen(!menuOpen);
+  };
   return (
     <nav className={styles.navWrapper}>
-      <ul className={styles.navLinks}>
+      <ul className={`${styles.navLinks} ${menuOpen ? styles.open : ""}`}>
         <li>
           <Link to="/projects">projects</Link>
         </li>
@@ -15,6 +21,11 @@ function Nav() {
           <Link to="/about">about</Link>
         </li>
       </ul>
+      <div className={styles.hamburgerMenu} onClick={toggleMenu}>
+        <span className={`${menuOpen ? styles.b1Open : ""}`} />
+        <span className={`${menuOpen ? styles.b2Open : ""}`} />
+        <span className={`${menuOpen ? styles.b3Open : ""}`} />
+      </div>
     </nav>
   );
 }
