475fb592db34d7f0c08748e34e0ae69f16fa20fa
jckstllrd <jackstallard17@gmail.com>
2026-07-08 12:09:36 +0100

    feat: render live link only if one is supplied as not every project will have a deployed frontend

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -13,9 +13,11 @@ function Project({ name, desc, liveLink, repoLink }: Props) {
       <h2>{name}</h2>
       <p>{desc}</p>
       <div className={styles.projectLinks}>
-        <a className={styles.links} href={liveLink}>
-          live
-        </a>
+        {liveLink && (
+          <a className={styles.links} href={liveLink}>
+            live
+          </a>
+        )}
         <a className={styles.links} href={repoLink}>
           repo
         </a>
