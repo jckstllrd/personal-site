ccd933a30b99e36895b320f57b44632049b3bf4c
jckstllrd <jackstallard17@gmail.com>
2026-07-09 11:06:03 +0100

    feat: break up about page copy into separate paragraphs

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,17 +1,23 @@
 import Container from "@/components/Container/Container";
+import styles from "./About.module.css";
 
 function About() {
   return (
     <Container>
-      <h1>about</h1>
-      <p>
-        I am a CS Grad that is currently rediscovering a passion for
-        programming. I work as an AI Strategy Consultant within the AI & Data
-        practice at Accenture for Capital Market clients, working across the
-        SDLC to improve and identify the best use cases for bringing in or
-        creating AI tooling. I hope to help developers know when and how to use
-        the various different embodiments of AI in their work.
-      </p>
+      <div className={styles.aboutPage}>
+        <h1>about</h1>
+        <p>
+          I am a CS Grad that is currently rediscovering a passion for
+          programming.
+        </p>
+        <p>
+          I work as an AI Strategy Consultant within the AI & Data practice at
+          Accenture for Capital Market clients, working across the SDLC to
+          improve and identify the best use cases for bringing in or creating
+          AI tooling. I hope to help developers know when and how to use the
+          various different embodiments of AI in their work.
+        </p>
+      </div>
     </Container>
   );
 }
