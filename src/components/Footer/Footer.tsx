abfdde3e8a80d0935d8dfc846e35a665c8249b6b
jckstllrd <jackstallard17@gmail.com>
2026-07-15 14:21:41 +0100

    feat: download removal

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -39,26 +39,6 @@ export default function Footer() {
             </svg>
           </a>
         </li>
-        <li>
-          <a
-            href="/jackstallard%20-%20CV.pdf"
-            aria-label="Download CV"
-            download
-          >
-            <svg className={styles.icon} viewBox="0 0 24 24">
-              <path
-                d="M8 3h6l4 4v12a2 2 0 0 1-2 2H8a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2z"
-                fill="currentColor"
-              />
-              <path
-                d="M14 3v4h4M9 13h6M9 17h6"
-                fill="none"
-                stroke="var(--color-background)"
-                strokeWidth="1.5"
-              />
-            </svg>
-          </a>
-        </li>
       </ul>
     </footer>
   );
